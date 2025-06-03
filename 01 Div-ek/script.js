d5e1e4d7b3774c88c738eaf95af303d2b65d11dc
nagy-imre <nagyimre@ktch.hu>
2025-06-03 21:32:37 +0200

    Második, negyedik kész

--- 01 Div-ek/script.js
+++ 01 Div-ek/script.js
@@ -1,6 +1,13 @@
 const divs = document.querySelectorAll("body > div");
 
 divs[0].textContent = "Első";
+
 divs[2].textContent = "Harmadik";
+
 divs[divs.length - 1].textContent = "Utolsó";
 
+for (let i = 0; i < divs.length; i++) {
+    if (i !== 0 && i !== 2 && i !== divs.length -1) {
+        divs[i].textContent = `${i + 1}`;
+    }
+}
